b0b28600898acf73207633b6048e653f119443a0
Chalaya22 <chalayaolga22@gmail.com>
2025-12-28 01:15:51 +0200

    изменения стиля хедера

--- script.js
+++ script.js
@@ -31,3 +31,56 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 });
+//switch
+// document.addEventListener("DOMContentLoaded", () => {
+//   const langToggle = document.querySelector(".lang-toggle");
+//   if (!langToggle) return;
+
+//   langToggle.addEventListener("click", () => {
+//     const currentLang = langToggle.dataset.lang;
+
+//     if (currentLang === "ro") {
+//       window.location.href = "../ru/index.html";
+//     } else {
+//       window.location.href = "../ro/index.html";
+//     }
+//   });
+// });
+function switchLang(lang) {
+  const path = window.location.pathname;
+  if (lang === "ru") {
+    window.location.href = path.replace("/ro/", "/ru/");
+  } else {
+    window.location.href = path.replace("/ru/", "/ro/");
+  }
+}
+
+/* desktop toggle */
+const toggle = document.querySelector(".lang-toggle");
+if (toggle) {
+  toggle.addEventListener("click", () => {
+    const current = toggle.dataset.lang;
+    const next = current === "ro" ? "ru" : "ro";
+    switchLang(next);
+  });
+}
+
+/* mobile buttons */
+
+const mobileToggle = document.querySelector(".mobile-lang-toggle");
+
+if (mobileToggle) {
+  mobileToggle.addEventListener("click", () => {
+    const current = mobileToggle.dataset.lang;
+    const next = current === "ro" ? "ru" : "ro";
+    switchLang(next);
+  });
+}
+
+const isRU = window.location.pathname.includes("/ru/");
+
+if (mobileToggle) {
+  mobileToggle.dataset.lang = isRU ? "ru" : "ro";
+  mobileToggle.querySelector(".ro").classList.toggle("active", !isRU);
+  mobileToggle.querySelector(".ru").classList.toggle("active", isRU);
+}
